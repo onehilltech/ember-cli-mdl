df26d65fb55d6e4ad231a7c45e2cdd8514e35e14
James H. Hill <hilljh82@gmail.com>
2017-09-24 01:36:36 -0400

    Added mdl-layout-drawer component

---
+++ addon/components/mdl-layout-drawer.js
@@ -0,0 +1,8 @@
+import Component from '../-private/component';
+import layout from '../templates/components/mdl-layout-drawer';
+
+export default Component.extend({
+  layout,
+
+  classNames: ['mdl-layout__drawer']
+});
