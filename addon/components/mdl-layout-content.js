7415a6c8302b29470f7b12cc21e838cf46dda394
James H. Hill <hilljh82@gmail.com>
2017-09-24 01:38:51 -0400

    Added mdl-layout-content component

---
+++ addon/components/mdl-layout-content.js
@@ -0,0 +1,10 @@
+import Component from '../-private/component';
+import layout from '../templates/components/mdl-layout-content';
+
+export default Component.extend({
+  layout,
+
+  tagName: 'main',
+
+  classNames: ['mdl-layout__content']
+});
