d64cbe4f3b8a45a33465daf5287b3f5afd5f9a19
James H. Hill <hilljh82@gmail.com>
2017-09-25 00:55:22 -0400

    Added ripple effect mixin

--- addon/components/mdl-tabs.js
+++ addon/components/mdl-tabs.js
@@ -1,9 +1,11 @@
 import Component from '../-private/component';
+import RippleEffectMixin from '../-private/mixins/ripple-effect-mixin';
+
 import layout from '../templates/components/mdl-tabs';
 
-export default Component.extend({
+export default Component.extend (RippleEffectMixin, {
   layout,
-
   classNames: ['mdl-tabs', 'mdl-js-tabs'],
-  classNameBindings: ['rippleEffect:mdl-js-ripple-effect']
+
+  rippleEffect: true
 });
