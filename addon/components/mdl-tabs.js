79f784df2dc682f76e0363b1f95d3d8553665dff
James H. Hill <hilljh82@gmail.com>
2017-09-25 11:41:44 -0400

    Automatically link tabs to panels

--- addon/components/mdl-tabs.js
+++ addon/components/mdl-tabs.js
@@ -5,7 +5,26 @@ import layout from '../templates/components/mdl-tabs';
 
 export default Component.extend (RippleEffectMixin, {
   layout,
+
   classNames: ['mdl-tabs', 'mdl-js-tabs'],
 
-  rippleEffect: true
+  rippleEffect: true,
+
+  didInsertElement () {
+    this._super (...arguments);
+    this._linkTabsToPanels ();
+  },
+
+  didUpdate () {
+    this._super (...arguments);
+    this._linkTabsToPanels ();
+  },
+
+  _linkTabsToPanels () {
+    let $tabs = this.$ ('.mdl-tabs__tab-bar > .mdl-tabs__tab');
+    let $panels = this.$ ('.mdl-tabs__panel');
+
+    for (let i = 0, len = $tabs.length; i < len; ++ i)
+      $tabs[i].href = `#${$panels[i].id}`;
+  }
 });
