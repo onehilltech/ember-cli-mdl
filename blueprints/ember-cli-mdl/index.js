54244549028b14dd36a27a02f52643108f78fa7e
James H. Hill <hilljh82@gmail.com>
2017-09-28 01:24:34 -0400

    Fixed blueprint definition

--- blueprints/ember-cli-mdl/index.js
+++ blueprints/ember-cli-mdl/index.js
@@ -1,10 +1,10 @@
 /* eslint-env node */
 module.exports = {
-  description: '',
+  normalizeEntityName: function() {}, // no-op since we're just adding dependencies
 
   afterInstall () {
     return this.addBowerPackagesToProject ([
-      {name: 'material-design-light', target: '^1.3.0'},
+      {name: 'material-design-lite', target: '^1.3.0'},
       {name: 'material-design-icons', target: '^3.0.1'}
     ]).then (() => {
       return this.addAddonsToProject({
