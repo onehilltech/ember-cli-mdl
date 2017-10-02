5fa89decf87b3b43eda4b751733b71d7d3095ab0
James H. Hill <hilljh82@gmail.com>
2017-10-02 09:37:15 -0400

    Added mdl-card-header component

---
+++ addon/components/mdl-card-header.js
@@ -0,0 +1,8 @@
+import SupportingText from '../components/mdl-card-supporting-text';
+import layout from '../templates/components/mdl-card-header';
+
+export default SupportingText.extend({
+  layout,
+
+  classNames: ['mdl-card__header']
+});
