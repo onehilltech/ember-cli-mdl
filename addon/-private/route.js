50853cca2cf2edb4ce6f9ceefdbb3943aac41f80
James H. Hill <hilljh82@gmail.com>
2017-10-01 21:19:37 -0400

    Implement route object that added route name as a class to body

---
+++ addon/-private/route.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend ({
+  classNameForRoute: Ember.computed ('routeName', function () {
+    let className = this.get ('routeName').dasherize ().replace (/[.|/]/, '-');
+    return `mdl-route__${className}`;
+  }),
+
+  activate () {
+    this._super (...arguments);
+
+    Ember.$('body').addClass (this.get ('classNameForRoute'));
+  },
+
+  deactivate () {
+    this._super (...arguments);
+
+    Ember.$('body').removeClass (this.get ('classNameForRoute'));
+
+  }
+})
--- addon/index.js
+++ addon/index.js
@@ -1,2 +1,3 @@
 export {default as Component} from './-private/component';
 export {default as LinkComponent} from './-private/link-component';
+export {default as Route} from './-private/route';
