4eaee5e840d383a5591248e672fb3dc3edd9105c
James H. Hill <hilljh82@gmail.com>
2017-10-04 08:18:08 -0400

    Export list item component

--- addon/index.js
+++ addon/index.js
@@ -1,3 +1,5 @@
 export {default as Component} from './-private/component';
 export {default as LinkComponent} from './-private/link-component';
 export {default as Route} from './-private/route';
+export {default as ListItem} from './components/mdl-list-item';
+
