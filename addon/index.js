a409b5103804477c0d255847fd16bd09839e4197
James H. Hill <hilljh82@gmail.com>
2017-10-04 22:26:39 -0400

    Started exporting some of the components from the add-on

--- addon/index.js
+++ addon/index.js
@@ -1,5 +1,43 @@
-export {default as Component} from './-private/component';
-export {default as LinkComponent} from './-private/link-component';
 export {default as Route} from './-private/route';
-export {default as ListItem} from './components/mdl-list-item';
+
+// Export the components. This allows third-party add-ons and application
+// components to extends the components.
+
+import BadgeComponent from './components/mdl-badge';
+import ButtonComponent from './components/mdl-button';
+
+import CardComponent from './components/mdl-card';
+import CardActionComponent from './components/mdl-card-action';
+import CardActionsComponent from './components/mdl-card-actions';
+import CardHeaderComponent from './components/mdl-card-header';
+import CardMediaComponent from './components/mdl-card-media';
+import CardMenuComponent from './components/mdl-card-menu';
+import CardSubtitleTextComponent from './components/mdl-card-subtitle-text';
+import CardSupportingTextComponent from './components/mdl-card-supporting-text';
+import CardTitleComponent from './components/mdl-card-title';
+import CardTitleTextComponent from './components/mdl-card-title-text';
+
+import LinkComponent from './-private/link-component';
+import ListItemComponent from './components/mdl-list-item';
+
+let MaterialComponent = {
+  Badge: BadgeComponent,
+  Button: ButtonComponent,
+
+  Card: CardComponent,
+  CardAction: CardActionComponent,
+  CardActions: CardActionsComponent,
+  CardHeader: CardHeaderComponent,
+  CardMedia: CardMediaComponent,
+  CardMenu: CardMenuComponent,
+  CardSubtitle: CardSubtitleTextComponent,
+  CardSupportingText: CardSupportingTextComponent,
+  CardTitle: CardTitleComponent,
+  CardTitleText: CardTitleTextComponent,
+
+  ListItem: ListItemComponent,
+  Link: LinkComponent,
+};
+
+export {MaterialComponent as Component};
 
