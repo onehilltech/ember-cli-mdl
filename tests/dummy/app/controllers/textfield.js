8835604b535e8f02abaaa09784981f9ad426f016
James H. Hill <hilljh82@gmail.com>
2017-10-04 22:06:19 -0400

    Fixed remaining issues with expandable text fields

--- tests/dummy/app/controllers/textfield.js
+++ tests/dummy/app/controllers/textfield.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   customError: {
     valueMissing: 'You are missing a value!'
-  }
+  },
+
+  title: Ember.computed ('search', function () {
+    let search = this.get ('search');
+
+    return Ember.isEmpty (search) ? 'Find a church' : null;
+  })
 });
