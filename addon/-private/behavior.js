72e3d6c63a42fa365a3846b7336c305bc9e4469c
James H. Hill <hilljh82@gmail.com>
2017-10-22 13:17:16 -0400

    First pass at base class for behaviors

---
+++ addon/-private/behavior.js
@@ -0,0 +1,41 @@
+import Ember from 'ember';
+
+/**
+ * Base class for all material design lite components. This class ensures that elements
+ * dynamically added to the application are upgraded.
+ */
+export default Ember.Object.extend ({
+  _component: null,
+
+  component: Ember.computed ({
+    set (key, value) {
+      if (Ember.isNone (value)) {
+        this.willDetachFromComponent ();
+      }
+
+      this._component = value;
+
+      if (!Ember.isNone (value)) {
+        this.didAttachToComponent ();
+      }
+    },
+
+    get () {
+      return this._component;
+    }
+  }),
+
+  /**
+   * The bahavior was attached to a component.
+   */
+  didAttachToComponent () {
+
+  },
+
+  /**
+   * The behavior will detach from the component.
+   */
+  willDetachFromComponent () {
+
+  }
+});
