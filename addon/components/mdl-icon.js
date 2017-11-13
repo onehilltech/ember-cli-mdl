fcb3cf5bdb4242a8ac51d03bba8d4213a4613017
James H. Hill <hilljh82@gmail.com>
2017-11-13 17:25:07 -0500

    mdl-icon now extends mdl-component

--- addon/components/mdl-icon.js
+++ addon/components/mdl-icon.js
@@ -1,7 +1,8 @@
-import Ember from 'ember';
+import Component from '../-private/component';
+
 import layout from '../templates/components/mdl-icon';
 
-const Icon = Ember.Component.extend({
+const Icon = Component.extend ({
   layout,
 
   tagName: 'i',
