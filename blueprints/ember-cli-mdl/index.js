20f363861f55272676e03327689642edde5f7bc0
James H. Hill <hilljh82@gmail.com>
2018-01-14 00:37:57 -0500

    Added dialog-polyfill for non-Chrome support

--- blueprints/ember-cli-mdl/index.js
+++ blueprints/ember-cli-mdl/index.js
@@ -7,7 +7,8 @@ module.exports = {
       {name: 'material-design-lite', target: '^1.3.0'},
       {name: 'material-design-icons', target: '^3.0.1'},
       {name: 'mdl-select-component', source: 'https://github.com/MEYVN-digital/mdl-selectfield.git#^0.0.8'},
-      {name: 'initial.js', source: 'https://github.com/onehilltech/initial.js.git#0.3.0'}
+      {name: 'initial.js', source: 'https://github.com/onehilltech/initial.js.git#0.3.0'},
+      {name: 'dialog-polyfill', target: '~0.4.9'}
     ]).then (() => {
       return this.addAddonsToProject({
         packages: [
